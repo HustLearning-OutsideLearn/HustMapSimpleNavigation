c937d6756c6ef66deafbf9c06602cef38afcf7f8
Do Hoang Khoi <92308124+KhoiDOO@users.noreply.github.com>
2022-07-03 09:29:24 +0700

    Update Data Preprocessing

---
+++ DataStructureBigProject/Graph.cpp
@@ -0,0 +1,25 @@
+#include "Graph.h"
+
+Graph::Graph(int V_count){
+    Vertice_Count = V_count;
+}
+
+void Graph::Graph_Adj_Matrix_Init(){
+    
+}
+
+void Graph::Graph_Adj_List_Init(){
+
+}
+
+void Graph::Graph_Show_Adj_Matrix(){
+
+}
+
+void Graph::Graph_Show_Adj_List(){
+
+}
+
+void Graph::AddEdge(int index, node * _node){
+
+}
---
+++ DataStructureBigProject/Graph.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <list>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+#ifndef Graph_H
+#define Graph_H
+
+struct node{
+    int index;
+    int x;
+    int y;
+};
+
+class Graph
+{
+private:
+    int Vertice_Count;
+    list<struct node> Adj_lst;
+    vector<vector<float>> Adj_Matrix;
+public:
+    //Graph Constructor
+    Graph(int V_count);
+
+    //Graph Init with Adjacency Matrix
+    void Graph_Adj_Matrix_Init();
+
+    //Graph Init with Adjacency List
+    void Graph_Adj_List_Init();
+
+    //Function: Showing Adjacency Matrix
+    void Graph_Show_Adj_Matrix();
+
+    //Function: Showing Adjacency List
+    void Graph_Show_Adj_List();
+
+    //Function: Add Edge to Graph -- Use when init graph with Adjacency List
+    void AddEdge(int index, struct node * _node);
+};
+
+#endif
